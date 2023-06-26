c395a151675f48ea425df004ffdb1d8302bda4c9
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-06-26 16:47:42 +0300

    kidney disease made

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ from streamlit_option_menu import option_menu
 from menu.Diabetes import diabetes_menu
 from menu.HeartDisease import heart_disease_menu
 from menu.Parkinson import parkinson_menu
+from menu.Kidney import kidney_menu
 import os
 
 
 with st.sidebar:
     st.title("Disease Prediction")
     st.write("Select the disease you want to predict")
-    selected_disease = option_menu("Select Disease", ["Diabetes", "Heart Disease", "Parkinson"],
-    icons=['droplet', 'heart', 'person'],
+    selected_disease = option_menu("Select Disease", ["Diabetes", "Heart Disease", "Parkinson", "Kidney Disease"],
+    icons=['droplet', 'heart', 'person', 'clipboard2-heart'],
     default_index=0)
 
 if selected_disease == "Diabetes":
@@ -23,3 +24,6 @@ elif selected_disease == "Heart Disease":
     
 elif selected_disease == "Parkinson":
     parkinson_menu()
+
+elif selected_disease == "Kidney Disease":
+    kidney_menu()
---
+++ menu/Kidney.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from tensorflow.keras.models import load_model
+import os
+
+disease_models_path = os.path.join(os.getcwd(), "disease_models")
+diabetes_model = load_model(os.path.join(disease_models_path, "kidney_model.h5"))
+
+def kidney_menu():
+    st.title("Kidney Disease Prediction using ML")
+    
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        sugar = st.number_input("Sugar", min_value=0.000, value=0.000, step=0.0001, format="%.4f")
+    
+    with col2:
+        albumin = st.number_input("Albumin", min_value=0, value=0, step=1)
+    
+    with col1:
+        serum_creatinine = st.number_input("Serum Creatinine", min_value=0.000, value=0.000, step=0.0001, format="%.4f")
+    
+    with col2:
+        hemoglobin = st.number_input("Hemoglobin", min_value=0.000, value=0.000, step=0.0001, format="%.4f")
+    
+    with col1:
+        PCV = st.number_input("Packed Cell Volume", min_value=0, value=0, step=1)
+    
+    with col2:
+        hypertension = st.number_input("Hypertension", min_value=0, value=0, step=1, max_value=1)
+    
+    kidney_prediction = ""
+    
+    if st.button("Kidney Disease Predict"):
+        kidney_prediction = diabetes_model.predict([[sugar, albumin, serum_creatinine, hemoglobin, PCV, hypertension]])
+        if kidney_prediction[0] == 1:
+            st.error("Person have Kidney Disease")
+        else:
+            st.success("Person don't have Kidney Disease")
