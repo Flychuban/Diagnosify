43f59e6ff68a406c52263973d9d4d2f0bf7b1ff1
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-07-06 11:19:39 +0300

    added malaria menu file

--- menu/Malaria.py
+++ menu/Malaria.py
@@ -10,18 +10,14 @@ from keras.applications.vgg19 import preprocess_input
 import keras.utils as image
 
 disease_models_path = os.path.join(os.getcwd(), "disease_models")
-pneumonia_model = load_model(os.path.join(disease_models_path, "pneumonia_model.h5"))
+malaria_model = load_model(os.path.join(disease_models_path, "pneumonia_model.h5"))
 
-
-def pneumonia_menu():
-    st.title("Pneumonia Disease Prediction using ML")
-    st.info('This is a example of using a trained ML model to make predictions for cancer segmentation on images.')
+def model_menu():
+    st.title("Disease Prediction using ML")
+    st.info('This is a example of using a trained ML model to make predictions for malaria classification on images.')
     
     
     uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "jfif","png"])
-    
-    # Choose the output format
-    # output_format = st.selectbox("Choose Output Format", ("JPG, JFIF, JPEG", "PNG"))
     
     if uploaded_file is not None:
                             
@@ -29,15 +25,15 @@ def pneumonia_menu():
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
         img_data = preprocess_input(x)
-        prediction = pneumonia_model.predict(img_data)
+        prediction = malaria_model.predict(img_data)
         
         print(f"Prediction: {prediction}")
         
         if prediction[0][1] < 0.5:
-            st.success("Person DON'T have Pneumonia!")
+            st.success("Person DON'T have Malaria!")
         
         else:
-            st.error("Person have Pneumonia!")
+            st.error("Person have Malaria!")
             
         image_plot = Image.open(uploaded_file)
         st.image(image_plot, caption='Uploaded Image', use_column_width=True)
