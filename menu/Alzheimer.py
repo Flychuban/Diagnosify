76ffc4d849c8c136b2188abbe5df5e95a0a6f631
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-07-17 11:17:45 +0300

    alzheimer menu

--- menu/Alzheimer.py
+++ menu/Alzheimer.py
@@ -8,29 +8,28 @@ from PIL import Image
 import keras.utils as image
 
 disease_models_path = os.path.join(os.getcwd(), "disease_models")
-malaria_model = load_model(os.path.join(disease_models_path, "malaria_model_cnn.h5"))
+alzheimer_model = load_model(os.path.join(disease_models_path, "malaria_model_cnn.h5"))
 
-def malaria_menu():
+
+def alzheimer_menu():
     st.title("Disease Prediction using ML")
-    st.info('This is a example of using a trained ML model to make predictions for malaria classification on images.')
-    
+    st.info('This is a example of using a trained ML model to make predictions for alzheimer classification on images.')
     
     uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "jfif","png"])
     
     if uploaded_file is not None:
-                            
         img = image.load_img(uploaded_file, target_size=(128, 128))
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        prediction = malaria_model.predict(x)
+        img_data = image.img_to_array(img)
+        img_data = np.expand_dims(img_data, axis=0)
+        
+        prediction = alzheimer_model.predict(img_data)
         
         print(f"Prediction: {prediction}")
         
-        if prediction[0] > 0.5:
-            st.success("Person DON'T have Malaria!")
-        
+        if prediction[0][1] > 0.5:
+            st.success("Person DON'T have Alzheimer!")
         else:
-            st.error("Person have Malaria!")
+            st.error("Person have Alzheimer!")
             
         image_plot = Image.open(uploaded_file)
         st.image(image_plot, caption='Uploaded Image', use_column_width=True)
