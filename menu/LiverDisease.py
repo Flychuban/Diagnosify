0a2587719cb78a8cabd65a2585ac71e11b73b548
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-07-26 19:05:06 +0300

    bug with gender

--- menu/LiverDisease.py
+++ menu/LiverDisease.py
@@ -13,6 +13,42 @@ def liver_disease_menu():
         age = st.number_input("Age", min_value=0, value=0, step=1)
     
     with col2:
-        option = st.selectbox(
+        gender = st.selectbox(
         'How would you like to be contacted?',
         ('Female', 'Male'))
+        
+        if gender == "Female":
+            gender = 0
+        else:
+            gender = 1
+    
+    with col3:
+        total_bilirubin = st.number_input("Total Bilirubin", min_value=0, value=0, step=1)
+        
+    with col1:    
+        direct_bilirubin = st.number_input("Direct Bilirubin", min_value=0, value=0, step=1)
+        
+    with col2:
+        alkaline_phosphotase = st.number_input("Alkaline Phosphotase", min_value=0, value=0, step=1)
+    
+    with col3:
+        alamine_aminotransferase = st.number_input("Alamine Aminotransferase", min_value=0, value=0, step=1)
+        
+    with col1:
+        aspartate_aminotransferase = st.number_input("Aspartate Aminotransferase", min_value=0, value=0, step=1)
+        
+    with col2:
+        total_protiens = st.number_input("Total Protiens", min_value=0, value=0, step=1)
+    
+    with col3:
+        albumin = st.number_input("Albumin", min_value=0, value=0, step=1)
+        
+    with col1:
+        ag_ratio = st.number_input("A/G Ratio", min_value=0.00, value=0.00, step=0.01)
+    
+    liver_disease_prediction = liver_disease_model.predict([age, gender, total_bilirubin, direct_bilirubin, alkaline_phosphotase, alamine_aminotransferase, aspartate_aminotransferase, total_protiens, albumin, ag_ratio])
+    
+    if liver_disease_prediction[0] == 1:
+        st.error("Person have Liver Disease")
+    else:
+        st.success("Person don't have Liver Disease")
