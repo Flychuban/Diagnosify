1fd21cdcfe67e785803abe9787c3b476aac8af78
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-07-27 22:39:02 +0300

    liver disease bug

--- menu/LiverDisease.py
+++ menu/LiverDisease.py
@@ -23,10 +23,10 @@ def liver_disease_menu():
             gender = 1
     
     with col3:
-        total_bilirubin = st.number_input("Total Bilirubin", min_value=0, value=0, step=1)
+        total_bilirubin = st.number_input("Total Bilirubin", min_value=0.00, value=0.00, step=0.01)
         
     with col1:    
-        direct_bilirubin = st.number_input("Direct Bilirubin", min_value=0, value=0, step=1)
+        direct_bilirubin = st.number_input("Direct Bilirubin", min_value=0.00, value=0.00, step=0.01)
         
     with col2:
         alkaline_phosphotase = st.number_input("Alkaline Phosphotase", min_value=0, value=0, step=1)
@@ -38,17 +38,23 @@ def liver_disease_menu():
         aspartate_aminotransferase = st.number_input("Aspartate Aminotransferase", min_value=0, value=0, step=1)
         
     with col2:
-        total_protiens = st.number_input("Total Protiens", min_value=0, value=0, step=1)
+        total_protiens = st.number_input("Total Protiens", min_value=0.00, value=0.00, step=0.01)
     
     with col3:
-        albumin = st.number_input("Albumin", min_value=0, value=0, step=1)
+        albumin = st.number_input("Albumin", min_value=0.00, value=0.00, step=0.01)
         
     with col1:
         ag_ratio = st.number_input("A/G Ratio", min_value=0.00, value=0.00, step=0.01)
     
-    liver_disease_prediction = liver_disease_model.predict([age, gender, total_bilirubin, direct_bilirubin, alkaline_phosphotase, alamine_aminotransferase, aspartate_aminotransferase, total_protiens, albumin, ag_ratio])
+    liver_disease_prediction = ""
     
-    if liver_disease_prediction[0] == 1:
-        st.error("Person have Liver Disease")
-    else:
-        st.success("Person don't have Liver Disease")
+    if st.button("Liver Disease Predict"):
+    
+        liver_disease_prediction = liver_disease_model.predict([[age, gender, total_bilirubin, direct_bilirubin, alkaline_phosphotase, alamine_aminotransferase, aspartate_aminotransferase, total_protiens, albumin, ag_ratio]])
+        
+        print(liver_disease_prediction)
+        
+        if liver_disease_prediction[0] == 0:
+            st.error("Person have Liver Disease")
+        else:
+            st.success("Person don't have Liver Disease")
