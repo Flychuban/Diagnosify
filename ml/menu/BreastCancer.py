a2fb4a4d290487cab6527be9e4c1292b8d6b88dc
Flychuban <kaloanas07@gmail.com>
2024-04-02 21:28:43 +0300

    add menus with scalers breast_cancer and diabetes

--- ml/menu/BreastCancer.py
+++ ml/menu/BreastCancer.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pickle
 import os
+import joblib
+import pandas as pd
+
+scaler_path = os.path.join(os.getcwd(), "scalers")
+scaler = joblib.load(os.path.join(scaler_path, "breast_cancer_scaler.pkl"))
+
 
 disease_models_path = os.path.join(os.getcwd(), "disease_models")
 breast_cancer_model = pickle.load(open(os.path.join(disease_models_path, "breast_cancer_model.sav"), 'rb'))
@@ -56,9 +62,16 @@ def breast_cancer_menu():
         concave_points_worst = st.number_input("Concave Points Worst", min_value=0.00000, value=0.00000, step=0.00001, format="%.5f")
 
 
+    # Combine all the features into a list and then scale them
+    data = [[radius_mean, perimeter_mean, area_mean, compactness_mean, concavity_mean, concave_points, radius_se, perimeter_se, area_se, radius_worst, perimeter_worst, area_worst, compactness_worst, concavity_worst, concave_points_worst]]
+    df_dummies = pd.DataFrame(data, columns=["radius_mean", "perimeter_mean", "area_mean", "compactness_mean", "concavity_mean", "concave points_mean", "radius_se", "perimeter_se", "area_se", "radius_worst", "perimeter_worst", "area_worst", "compactness_worst", "concavity_worst", "concave points_worst"])
+    column_names = df_dummies.columns
+    df_dummies[column_names] = scaler.transform(df_dummies[column_names])
+
+    breast_cancer_prediction = ""
 
     if st.button("Breast Cancer Predict"):
-        breast_cancer_prediction = breast_cancer_model.predict([[radius_mean, perimeter_mean, area_mean, compactness_mean, concavity_mean, concave_points, radius_se, perimeter_se, area_se, radius_worst, perimeter_worst, area_worst, compactness_worst, concavity_worst, concave_points_worst]])
+        breast_cancer_prediction = breast_cancer_model.predict(df_dummies[["radius_mean", "perimeter_mean", "area_mean", "compactness_mean", "concavity_mean", "concave points_mean", "radius_se", "perimeter_se", "area_se", "radius_worst", "perimeter_worst", "area_worst", "compactness_worst", "concavity_worst", "concave points_worst"]])
         if breast_cancer_prediction[0] == 1:
             st.error("Person have Breast Cancer")
         else:
