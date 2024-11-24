a047cc91ee5230cbb218c69f6148c76656b352a3
RadoslavTsvetanov <radoslavcvetanov5@gmail.com>
2024-11-24 16:14:33 +0200

    made endpoit for diabetes data collection and the general structure

--- web/backend/ml_data_gathering_service/src/main.ts
+++ web/backend/ml_data_gathering_service/src/main.ts
@@ -1,53 +1,89 @@
+import bodyParser  from 'body-parser';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import AWS from "aws-sdk"
 
-interface MLRoute {
-  ml_type: string;
-  data_collection_handler: (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ) => void;
+enum requestCompletionStatus{
+  OK,
+  ERRORED
 }
 
-function createExpressServer(
-  mlRoutes: MLRoute[],
-  port: number,
-): express.Application {
-  const app = express();
-  app.use(cors());
+const app = express()
+app.use(cors())
+app.use(bodyParser.json());
+const { S3_USER_CLIENT, S3_USER_SECRET, S3_BUCKET_NAME, S3_REGION } =
+  process.env;
 
-  mlRoutes.forEach((route) => {
-    app.post(`/ml/${route.ml_type}`, route.data_collection_handler);
-  });
-
-  app.listen(port, () => {
-    console.log(`Express server listening on port ${port}`);
-  });
-
-  return app;
+if (!S3_USER_CLIENT || !S3_USER_SECRET || !S3_BUCKET_NAME || !S3_REGION) {
+  throw new Error("Missing required S3 environment variables");
 }
 
-function create_ml_handler<T>(handler(request: express.Request, response: express.Response<{}>)) {
+const s3 = new AWS.S3({
+  accessKeyId: S3_USER_CLIENT,
+  secretAccessKey: S3_USER_SECRET,
+  region: S3_REGION,
+});
 
+const dataCollectionRouter = express.Router();
+
+function createRoute<T>(route: string, handler: (data: T) => { status: requestCompletionStatus, err?: string, dataForS3Upload?: { Bucket: string,Key: string, Body: Buffer, ContentType: string } }) {
+  dataCollectionRouter.post(route,async (req: express.Request<{}, {}, T>, res: express.Response<{ status: requestCompletionStatus, err: string | null }>) => {
+    const result = handler(req.body);
+    if (result.status === requestCompletionStatus.OK) {
+
+      const uploadResult = await s3.upload(result.dataForS3Upload).promise();
+      res.status(200).json({ status: requestCompletionStatus.OK, err: null });
+      return;
+    } else {
+      res.status(500).json({ status: requestCompletionStatus.ERRORED, err: result.err });
+      return
+    }
+  })
 }
 
+function generateFileName() {
+  const timestamp = new Date().getTime();
+  return `${timestamp}.json`;
+}
 
-const handlers: MLRoute[] = [
-  {
-    ml_type: 'handler1`',
-    data_collection_handler: (req: express.Request, res: express.Response) => {
-      console.log('Received data for handler1:', req.body);
-      res.send('Data received for handler1');
-    },
-  },
-  {
-    ml_type: 'handler2',
-    data_collection_handler: (req, res) => {
-      console.log('Received data for handler2:', req.body);
-      res.send('Data received for handler2');
-    },
-  },
-];
+createRoute<{Pregnancies: string,
+    Glucose: string,
+    BloodPressure: string,
+    SkinThickness: string,
+    Insulin: string,
+    BMI: string,
+    DiabetesPedigreeFunction: string,
+  Age: string
+}>("/diabetes", (data => {
+     try {
 
-createExpressServer(handlers, 5000);
+        if (!data) {
+          return { status: requestCompletionStatus.ERRORED, err: "Data is missing" };
+        }
+
+        // Create an in-memory file using Buffer
+        const fileBuffer = Buffer.from(JSON.stringify(data), 'utf-8');
+
+        // Upload to S3
+        const params = {
+            Bucket: S3_BUCKET_NAME,
+            Key: `diag/diabetes/${generateFileName()}`, // File name in S3
+            Body: fileBuffer,
+            ContentType: 'text/plain',
+            ACL: "public-read",
+        };
+
+        return { status: requestCompletionStatus.OK, dataForS3Upload: params };
+    } catch (error) {
+        console.error('Upload error:', error);
+      return { status: requestCompletionStatus.ERRORED, err: error };
+     } 
+}))
+    
+
+app.use("/data", dataCollectionRouter)
+
+const port = 4001
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+})
