ab6c282f3f3487a87fe4c26c8f68dd4ec1a7c4f7
radi2007 <radoslav.a.tsvetanov.2021@elsys-bg.org>
2024-12-07 20:18:03 +0000

    ji;

--- web/frontend/src/pages/index.tsx
+++ web/frontend/src/pages/index.tsx
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
-
+import {useEffect} from "react"
 export default function Home() {
-  return (
+	useEffect(() => {
+		window.location.href="/diagnoses/new"
+	})
+	return (
     <div className="text-primarytext">
        abiut page ask kala to give you ai genrated page with info about the project
    </div> 
