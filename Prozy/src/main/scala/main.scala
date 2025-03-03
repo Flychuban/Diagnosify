9208b315529829f02e1ed1ae00b8355618e9dfdb
radi2007 <radoslav.a.tsvetanov.2021@elsys-bg.org>
2025-03-03 18:59:42 +0000

    fixed proxy cors issues

--- Prozy/src/main/scala/main.scala
+++ Prozy/src/main/scala/main.scala
@@ -4,6 +4,13 @@ import types.CORSConfig
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.{HttpRoutes, Method, Request, Response, Uri}
 import webserver.{IMiddleware, ReverseProxy}
+import org.http4s.{Headers, Status}
+import org.http4s.implicits._
+import cats.data.OptionT
+import cats.effect.IO
+import org.http4s._
+import org.http4s.headers._
+
 
 enum Service(val name: String, val url: String):
   case AUTH extends Service("auth", "http://localhost:8080")
@@ -26,38 +33,52 @@ def resolveService(req: Request[IO]): String =
     .map(_.url)
     .getOrElse(Service.UNKNOWN.url)
 
-class OptionsRequestMiddleware extends IMiddleware {
-  override def apply(routes: HttpRoutes[IO]): HttpRoutes[IO] = {
+class CorsMiddleware extends IMiddleware {
+  def apply(routes: HttpRoutes[IO]): HttpRoutes[IO] = {
     HttpRoutes { request =>
-      if (request.method == Method.OPTIONS) {
-        OptionT.some(Response[IO](status = org.http4s.Status.Ok))
-      } else {
-        routes(request)
+      routes(request).map { response =>
+        response.putHeaders(
+          "Access-Control-Allow-Origin" -> "*",
+          "Access-Control-Allow-Methods" -> "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers" -> "Content-Type, Authorization",
+          "Access-Control-Allow-Credentials" -> "true"
+        )
+      }.orElse {
+        // Handle OPTIONS requests directly
+        if (request.method == Method.OPTIONS) {
+          OptionT.some(Response[IO](status = org.http4s.Status.Ok).putHeaders(
+            "Access-Control-Allow-Origin" -> "*",
+            "Access-Control-Allow-Methods" -> "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers" -> "Content-Type, Authorization",
+            "Access-Control-Allow-Credentials" -> "true"
+          ))
+        } else {
+          OptionT.none
+        }
       }
     }
   }
 }
 
-
-
 object Main extends IOApp.Simple:
   println("running server")
-
   def run: IO[Unit] =
     EmberClientBuilder.default[IO].build.use { client =>
       ReverseProxy()
         .withResolver { req =>
           val baseUrl = resolveService(req)
-          println("resloved")
+          println("resolved")
           println(baseUrl)
           Uri.unsafeFromString(baseUrl).withPath(req.uri.path)
         }
-        .addCors(CORSConfig())
-        .addMiddleware(new OptionsRequestMiddleware())
+        .withSSL(
+          keystorePath = "/root/flask-reverse-2/keystore.p12",
+          keystorePassword = "changeit",
+          keyManagerPassword = "changeit"
+        )
+        .addMiddleware(new CorsMiddleware()) // Replace addCors with the new middleware
         .withLogging(req => IO.println(s"Proxying request: ${req.method} ${req.uri}"))
         .build()
-        .listen(port = 3003, host = "0.0.0.0", client)
+        .listen(port = 2053, host = "0.0.0.0", client)
         .as(())
     }
-
-
